b94924c7e708f8cd433e311dbb93fbc88505431d
Matthias Hinrichs <matthias.hinrichs@me.com>
2024-06-29 17:51:45 +0200

    added styles for waybar and swaylock

---
+++ install_opensuse.sh
@@ -0,0 +1,15 @@
+# This script installs the required files to run hyprland on openSuse Tumbleweed
+
+PACKAGES="git kitty zsh hyprland hyprcursor hyprpaper swaybar"
+
+
+# Install packages
+
+sudo zypper -n install $PACKAGES
+
+# Install fonts
+
+cd ~/.local/share/fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Meslo.zip
+unzip Meslo.zip
+rm Meslo.zip
